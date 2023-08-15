2d2616ff39798c0a8cc510700504373169a4786d
raagul1234 <99135476+raagul1234@users.noreply.github.com>
2023-08-15 11:34:50 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,13 @@
+class Anime:
+    def __init__(self,name,genre,rating):
+        self.name=name
+        self.genre=genre
+        self.rating=rating
+    def get_name(self):
+        return self.name
+    def get_genre(self):
+        return self.genre
+    def get_rating(self):
+        return self.rating
+a=Anime
+        
